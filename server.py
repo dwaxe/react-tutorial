49a66934aeb54852b142724e83fd77796621fefd
Darwin <DarwinWhy@gmail.com>
2016-04-14 18:41:06 -0700

    more random edits

--- server.py
+++ server.py
@@ -15,6 +15,8 @@ from flask import Flask, Response, request
 
 app = Flask(__name__, static_url_path='', static_folder='public')
 app.add_url_rule('/', 'root', lambda: app.send_static_file('index.html'))
+app.add_url_rule('/study', 'study', lambda: app.send_static_file('study.html'))
+
 
 @app.route('/api/comments', methods=['GET', 'POST'])
 def comments_handler():
@@ -33,4 +35,4 @@ def comments_handler():
     return Response(json.dumps(comments), mimetype='application/json', headers={'Cache-Control': 'no-cache', 'Access-Control-Allow-Origin': '*'})
 
 if __name__ == '__main__':
-    app.run(port=int(os.environ.get("PORT",3000)))
+    app.run(port=int(os.environ.get("PORT", 3000)))
